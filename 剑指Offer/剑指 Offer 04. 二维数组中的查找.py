da4d6792dadaa5b26fc02f254f02b9f61df270dd
BoatInTheRiver <4302791@qq.com>
2020-08-06 10:38:02 +0800

    code_algorithm

---
+++ 剑指Offer/剑指 Offer 04. 二维数组中的查找.py
@@ -0,0 +1,35 @@
+# coding:utf-8
+'''
+在一个 n * m 的二维数组中，
+每一行都按照从左到右递增的顺序排序，每一列都按照从上到下递增的顺序排序。
+请完成一个函数，输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
+
+示例:
+
+现有矩阵 matrix 如下：
+
+[
+  [1,   4,  7, 11, 15],
+  [2,   5,  8, 12, 19],
+  [3,   6,  9, 16, 22],
+  [10, 13, 14, 17, 24],
+  [18, 21, 23, 26, 30]
+]
+给定 target = 5，返回 true。
+
+给定 target = 20，返回 false。
+
+'''
+class Solution:
+    def findNumberIn2DArray(self, matrix, target):
+        if not matrix:
+            return False
+        i, j = 0, len(matrix[0]) - 1
+        while i < len(matrix) and j >= 0:
+            if matrix[i][j] > target:
+                j -= 1
+            elif matrix[i][j] < target:
+                i += 1
+            else:
+                return True
+        return False

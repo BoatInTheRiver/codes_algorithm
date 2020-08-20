8d1c956f13ab0ac37ea03275c4ba60d871309885
BoatInTheRiver <4302791@qq.com>
2020-08-20 22:17:21 +0800

    code_algorithm

---
+++ leetcode/linkedList/86. 分隔链表.py
@@ -0,0 +1,32 @@
+# coding:utf-8
+
+'''
+给定一个链表和一个特定值 x，对链表进行分隔，使得所有小于 x 的节点都在大于或等于 x 的节点之前。
+你应当保留两个分区中每个节点的初始相对位置。
+
+示例:
+输入: head = 1->4->3->2->5->2, x = 3
+输出: 1->2->2->4->3->5
+'''
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def partition(self, head, x):
+        dummy1 = ListNode(0)
+        dummy2 = ListNode(0)
+        p1, p2 = dummy1, dummy2
+        while head:
+            if head.val < x:
+                p1.next = head
+                p1 = p1.next
+            else:
+                p2.next = head
+                p2 = p2.next
+            head = head.next
+        p1.next = dummy2.next
+        p2.next = None
+        return dummy1.next

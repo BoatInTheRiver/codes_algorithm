c646cb6c5c3fd225f8d01115d4d758ec22425335
BoatInTheRiver <4302791@qq.com>
2020-09-02 17:41:14 +0800

    code_algorithm

--- 剑指Offer/剑指 Offer 46. 把数字翻译成字符串.py
+++ 剑指Offer/剑指 Offer 46. 把数字翻译成字符串.py
@@ -15,10 +15,9 @@ class Solution:
         s = str(num)
         n = len(s)
         dp = [1 for _ in range(n + 1)]
-        dp[0] = dp[1] = 1
         for i in range(2, n + 1):
             if '10' <= s[i - 2:i] <= '25':
                 dp[i] = dp[i - 1] + dp[i - 2]
             else:
                 dp[i] = dp[i - 1]
-        return dp[-1]
+        return dp[n]
